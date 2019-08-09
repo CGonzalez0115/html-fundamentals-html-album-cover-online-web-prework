9db69432f8a3e8d04225531deb94225bef2bf1f8
Christina Gonzalez <cmgamway@gmail.com>
2019-08-09 09:26:10 +0000

    Automatically backed up by Learn

--- spec/album_cover_spec.rb
+++ spec/album_cover_spec.rb
@@ -17,20 +17,7 @@ RSpec.describe 'HTML Album Cover' do
 
       it 'the back section contains the correct header sizes and text content' do
         back_cover = parsed_html.search('#back').first
-
-        expect(back_cover.children.any? {|ch| ch.name == "p"}).to be == true, "No 'p' tag found"
-        expect(back_cover.children.select {|ch| ch.name == "p"}.first.children.first.text).to include("PUBLISHED BY"), "The 'p' tag should include the text 'PUBLISHED BY'"
-
-        expect(back_cover.children.any? {|ch| ch.name == "span"}).to be == true, "No 'span' tag was found"
-        expect(back_cover.children.select {|ch| ch.name == "span"}.first.children.first.text).to include("PHARRELLWILLIAMS.COM"), "The first span tag should include the text 'PHARRELLWILLIAMS.COM'"
-
-        expect(back_cover.children.any? {|ch| ch.name == "img"}).to be == true, "No 'img' tag was found"
-
-        expect(back_cover.children.select {|ch| ch.name == "img"}[0].attributes["src"].value).to be == 'https://s3.amazonaws.com/learn-verified/columbia-logo.png', "First image should have its source set to 'https://s3.amazonaws.com/learn-verified/columbia-logo.png'"
-
-        expect(back_cover.children.select {|ch| ch.name == "span"}[1].children.first.text).to include("COLUMBIARECORDS.COM"), "The second span tag should include the text 'COLUMBIARECORDS.COM'"
-
-        expect(back_cover.children.select {|ch| ch.name == "img"}[1].attributes["src"].value).to be == 'https://s3.amazonaws.com/learn-verified/sony-logo.png', "First image should have its source set to 'https://s3.amazonaws.com/learn-verified/columbia-logo.png'"
+ct(back_cover.children.select {|ch| ch.name == "img"}[1].attributes["src"].value).to be == 'https://s3.amazonaws.com/learn-verified/sony-logo.png', "First image should have its source set to 'https://s3.amazonaws.com/learn-verified/columbia-logo.png'"
 
       end
 
