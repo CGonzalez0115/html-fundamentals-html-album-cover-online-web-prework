4363c99075fda0d06ba09437a028b2b680189c87
Christina Gonzalez <cmgamway@gmail.com>
2019-08-09 09:23:14 +0000

    Automatically backed up by Learn

--- spec/album_cover_spec.rb
+++ spec/album_cover_spec.rb
@@ -18,9 +18,6 @@ RSpec.describe 'HTML Album Cover' do
       it 'the back section contains the correct header sizes and text content' do
         back_cover = parsed_html.search('#back').first
 
-        expect(back_cover.children.any? {|ch| ch.name == "h3"}).to be == true, "No 'h3' tag found"
-        expect(back_cover.children.select {|ch| ch.name == "h3"}[0].children.first.text).to include("FROM DESPICABLE ME"), "An 'h3' tag should contain the text 'FROM DESPICABLE ME'"
-
         expect(back_cover.children.any? {|ch| ch.name == "h1"}).to be == true, "No 'h1' tag found"
         expect(back_cover.children.select {|ch| ch.name == "h1"}.first.children.first.text).to be == "HAPPY", "The `h1` tag should include the text 'HAPPY'"
 
